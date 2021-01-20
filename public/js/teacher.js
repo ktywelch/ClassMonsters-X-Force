11827d5b3de2b2892bd862063057f36d8d34bcdc
5of9 <KTYWELCH@GMAIL.COM>
2021-01-19 18:45:06 -0800

    Revert "whateer"

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -1,4 +1,3 @@
-
 let newMessages;
 let newMessageAlert;
 let snedMessageBtn;
@@ -54,40 +53,3 @@ const getNotes = (id) =>
       'Content-Type': 'application/json',
     },
   });
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM loaded')
-})
-
-const addTeacher = (teacherData) => {
-    fetch ('/api/teachers', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(teacherData)
-    }).then(getTeacher)
-        .catch((err) => console.error(err))
-}
-
-const getTeacher = () => {
-    console.log('Get Teacher is getting called');
-    fetch('/api/teachers', {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-    })
-        .then((response) => response.json())
-        // .then((data) => {
-        // // console.log('Success in getting authors:', authors);
-        // const rowsToAdd = [];
-        // for (let i = 0; i < data.length; i++) {
-        //     rowsToAdd.push(createTeacherRow(data[i]));
-        // }
-        // renderTeacherList(rowsToAdd);
-        // nameInput.value = '';
-        // })
-        // .catch((error) => console.error('Error:', error));
-};
-
