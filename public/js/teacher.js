e0eafe2d439410857a6b3ab4cfa1b3352116634a
Kathleen Welch <ktywelch@gmail.com>
2021-01-24 11:52:58 -0800

    added the dropdown for addmin

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -22,6 +22,72 @@ let begHtml =`<div class="row row-space-2" >
 
 
 // Teacher Functions
+
+const renderStudents = (students) => {
+  let  allStuHtml= "";
+  students.forEach(st => {   
+    let nickname = st.nickname;
+    if (st.nickname === null){
+        nickname = st.first_name;
+    }
+    let newHtml =`
+                <div class="col-md-6">
+                  <div class="card mb-2 mx-auto" style="width: 18rem;">
+                  <div class="text-center">
+                  <img src="./images/${st.Character.filename}" alt="${st.Character.alt_txt}" 
+                     class="img-responsive profile-image" 
+                     />
+                  <h5 class="card-title">Student Information</h5>
+                  <div class="text-left" >
+                      <p>Name: ${st.first_name}  ${st.last_name}</p>
+                      <p>Prefers: ${nickname} </p>
+                      <p>Bio: </p>
+                      <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                          <div class="dropdown">
+                          <button class="btn btn-sm btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Class Tools</button>
+                              <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                  <button class="dropdown-item" type="button" id="Send_Message" value="${st.id}">Send Message to Student</button>
+                                  <button class="dropdown-item" type="button" id="Send_Message" value="${st.id}">Send Message to Student</button>
+                                  <button class="dropdown-item" type="button" id="Delete_Student" value="${st.id}">Deleter Student</button> 
+                                  <button class="dropdown-item" type="button" id="Change_Teacher" value="${st.id}">Change Teacher</button>    
+                              </div>
+                            </div>
+                          </div> 
+                          <div class="btn-group">
+                            <div class="dropdown">
+                             <button class="btn btn-sm btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Admin Tools</button>
+                          <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                            <button class="dropdown-item"> <a href="mailto:${st.parentEmail}">Send an e-mail to parent</a></button>
+                          </div>
+                        </div>
+                       </div> 
+                      </div> 
+                        <small class="text-muted"></small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+    `
+  allStuHtml += newHtml; 
+  // console.log (stuHtml)        
+  });
+  let locCount = document.querySelector('#stuCount');
+  let stuInfo = document.querySelector('#stu-info')
+  let newDiv1 = document.createElement("div");
+  let newDiv = document.createElement("div");
+  let headHtml = `<h4 class="m-t-0 m-b-20"> Student List (${students.length})</h4>`
+  newDiv1.innerHTML = headHtml;
+  locCount.appendChild(newDiv1)
+  newDiv.innerHTML = allStuHtml;
+  stuInfo.appendChild(newDiv);
+}
+
+
+
+
 const getStudents = (t_id,cb) => {
   console.log("getting Students");
   fetch(`/api/students/${t_id}`, {
@@ -44,67 +110,18 @@ getUserInfo(uid, user =>  {
   lname = user.last_name;
   fname = user.first_name;
   let teach_place = document.querySelector('#teacher_img')
-  let newD = document.createElement("div");
+  let newD2 = document.createElement("div");
   let newHtml = `<img src="./images/${user.Character.filename}" alt="${user.Character.alt_text}">`
-  newD.innerHTML=newHtml
-  teach_place.appendChild(newD); 
+  newD2.innerHTML=newHtml
+  teach_place.appendChild(newD2); 
   fn_loc = document.querySelector('#user_full_name');
   fn_loc.innerText = `${fname} ${lname}`
-  newD="";
+  newD2="";
 });
  
 if (window.location.pathname === '/teacher') {
     //these are from common & message js since they are common to students and teachers
-    let allStuHtml = "";
     getMess(uid);
     getAndRendMessages(uid);
-    getStudents(uid, students =>{
-      let newD = document.createElement("div");
-      let headHtml = `<h4 class="m-t-0 m-b-20"> Student List (${students.length})</h4>`
-      students.forEach(st => {   
-        let nickname = st.nickname;
-        if (st.nickname === null){
-            nickname = st.first_name;
-        }
-        let newHtml =`
-                    <div class="col-md-4">
-                      <div class="card mb-4 box-shadow">
-                      <div class="float-md-left">
-                      <img src="./images//${st.Character.filename}" alt="${st.Character.alt_txt}" 
-                         class="p-3 img-responsive profile-image float-md-left float-sm-left w-25" 
-                         />
-                      <h5 class="card-title">Student Information</h5>
-                      <div class="text-left" >
-                          <p>Name: ${st.first_name}  ${st.last_name}</p>
-                          <p>Prefers: ${nickname} </p>
-                          <p>Bio: </p>
-                          <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                              <div class="dropdown">
-                              <button class="btn btn-sm btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              Class Tools</button>
-                                  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                                      <button class="dropdown-item" type="button" id="Send_Message" value="${st.id}">Send Message to Student</button>
-                                      <button class="dropdown-item"> <a href="mailto:${st.parentEmail}">Send an e-mail to parent</a></button>
-                                      <button class="dropdown-item" type="button" id="Change_Character" value="${st.id}">Change Character</button> 
-                                      <button class="dropdown-item" type="button" id="Change_Teacher" value="${st.id}">Change Teacher</button>    
-                                  </div>
-                                </div>
-                              </div> 
-
-                              <button type="button" class="btn btn-sm btn-outline-secondary">Admin Tools</button>
-                            </div>
-                            <small class="text-muted"></small>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-        `
-      allStuHtml += newHtml; 
-      // console.log (stuHtml)        
-      });
-      let stuInfo = document.querySelector('#stu-info')
-      newD.innerHTML = headHtml + allStuHtml;
-      stuInfo.appendChild(newD);
-    })
+    getStudents(uid, students => renderStudents(students));
 }
