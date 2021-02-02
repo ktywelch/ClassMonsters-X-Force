2bc328936fa941097480ca36d1165b06d2a51065
Kathleen Welch <ktywelch@gmail.com>
2021-02-01 17:00:21 -0800

    updated readme

--- seeders/seeder.js
+++ seeders/seeder.js
@@ -23,12 +23,15 @@ module.exports = {
     {username: 'msmith', password: 'teacher', last_name: 'Smith', first_name: 'Maggie',  RoleId: role1[0][0].id},
     {username: 'eli', password: 'teacher', last_name: 'Li', first_name: 'Elaine',  RoleId: role1[0][0].id},
     {username: 'mtobias', password: 'teacher', last_name: 'Tobias', first_name: 'Margie',  RoleId: role1[0][0].id},
-    {username: 'jryan', password: 'student', last_name: 'Ryan', first_name: 'Jane',  RoleId: role2[0][0].id}, 
-    {username: 'tmoreno', password: 'student', last_name: 'Moreno', first_name: 'Tom',  RoleId: role2[0][0].id},
-    {username: 'kcat', password: 'student', last_name: 'Cat', first_name: 'Katherine',  RoleId: role2[0][0].id},
-    {username: 'mmouse', password: 'student', last_name: 'Mouse', first_name: 'Mindy',  RoleId: role2[0][0].id}, 
-    {username: 'pholiday', password: 'student', last_name: 'Holiday', first_name: 'Peter',  RoleId: role2[0][0].id},
-    {username: 'mjones', password: 'student', last_name: 'Jones', first_name: 'Matina',  RoleId: role2[0][0].id}
+    {username: 'jryan', password: 'student', last_name: 'Ryan', first_name: 'Jane',  RoleId: role2[0][0].id, TeacherId: '1'}, 
+    {username: 'tmoreno', password: 'student', last_name: 'Moreno', first_name: 'Tom',  RoleId: role2[0][0].id, TeacherId: '1'},
+    {username: 'kcat', password: 'student', last_name: 'Cat', first_name: 'Katherine',  RoleId: role2[0][0].id, TeacherId: '1'},
+    {username: 'mmouse', password: 'student', last_name: 'Mouse', first_name: 'Mindy',  RoleId: role2[0][0].id, TeacherId: '2'}, 
+    {username: 'pholiday', password: 'student', last_name: 'Holiday', first_name: 'Peter',  RoleId: role2[0][0].id, TeacherId: '2'},
+    {username: 'mjones', password: 'student', last_name: 'Jones', first_name: 'Matina',  RoleId: role2[0][0].id, TeacherId: '2'},
+    {username: 'tpots', password: 'student', last_name: 'Pots', first_name: 'Tim',  RoleId: role2[0][0].id, TeacherId: '3'}, 
+    {username: 'pglass', password: 'student', last_name: 'Glass', first_name: 'Peter',  RoleId: role2[0][0].id, TeacherId: '3'},
+    {username: 'mbook', password: 'student', last_name: 'Book', first_name: 'Monica',  RoleId: role2[0][0].id, TeacherId: '3'}
     ],{});
   
 
@@ -45,7 +48,10 @@ await queryInterface.bulkInsert('characters',[
   {name: 'Hulk', filename: 'hulk.png', alt_text: 'cartoon image of caped hero from Etsy'},
   {name: 'rainbow', filename: 'PlaceHolderRainbow.png',alt_text: 'cute clipart png from pngtree.com',UserId:'1'},
   {name: 'rainbow', filename: 'PlaceHolderRainbow.png',alt_text: 'cute clipart png from pngtree.com',UserId:'2'},
-  {name: 'rainbow', filename: 'PlaceHolderRainbow.png',alt_text: 'cute clipart png from pngtree.com',UserId:'3'}
+  {name: 'rainbow', filename: 'PlaceHolderRainbow.png',alt_text: 'cute clipart png from pngtree.com',UserId:'3'},
+  {name: 'flying_purple', filename:  'ClipartKey_1586159.png',alt_text:'cartoon image of caped hero from ClipartKey',UserId:'10'},
+  {name: 'Super_girl', filename:  'super_girl.jpg',alt_text:'cartoon image of caped hero from Etsy',UserId:'12'},
+  {name: 'Hulk', filename:  'hulk.png',alt_text:'cartoon image of caped hero from Etsy',UserId:'11'},
   ],{});
  }
 }
