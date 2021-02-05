64bce41eee9b86900ac1c47a61ba053c74a99b0d
Kathleen Welch <ktywelch@gmail.com>
2021-02-05 08:35:08 -0800

    fixed seeding js

--- seeders/seeder.js
+++ seeders/seeder.js
@@ -6,18 +6,18 @@ module.exports = {
   up: async (queryInterface) => {
 
     await db.Users.bulkCreate( [
-    {username: 'msmith', password: 'teacher', last_name: 'Smith', first_name: 'Maggie',  RoleId: role1[0][0].id},
-    {username: 'eli', password: 'teacher', last_name: 'Li', first_name: 'Elaine',  RoleId: role1[0][0].id},
-    {username: 'mtobias', password: 'teacher', last_name: 'Tobias', first_name: 'Margie',  RoleId: role1[0][0].id},
-    {username: 'jryan', password: 'student', last_name: 'Ryan', first_name: 'Jane',  RoleId: role2[0][0].id, TeacherId: '1'}, 
-    {username: 'tmoreno', password: 'student', last_name: 'Moreno', first_name: 'Tom',  RoleId: role2[0][0].id, TeacherId: '1'},
-    {username: 'kcat', password: 'student', last_name: 'Cat', first_name: 'Katherine',  RoleId: role2[0][0].id, TeacherId: '1'},
-    {username: 'mmouse', password: 'student', last_name: 'Mouse', first_name: 'Mindy',  RoleId: role2[0][0].id, TeacherId: '2'}, 
-    {username: 'pholiday', password: 'student', last_name: 'Holiday', first_name: 'Peter',  RoleId: role2[0][0].id, TeacherId: '2'},
-    {username: 'mjones', password: 'student', last_name: 'Jones', first_name: 'Matina',  RoleId: role2[0][0].id, TeacherId: '2'},
-    {username: 'tpots', password: 'student', last_name: 'Pots', first_name: 'Tim',  RoleId: role2[0][0].id, TeacherId: '3'}, 
-    {username: 'pglass', password: 'student', last_name: 'Glass', first_name: 'Peter',  RoleId: role2[0][0].id, TeacherId: '3'},
-    {username: 'mbook', password: 'student', last_name: 'Book', first_name: 'Monica',  RoleId: role2[0][0].id, TeacherId: '3'}
+    {username: 'msmith', password: 'teacher', last_name: 'Smith', first_name: 'Maggie',  RoleId: '1'},
+    {username: 'eli', password: 'teacher', last_name: 'Li', first_name: 'Elaine',  RoleId: '1'},
+    {username: 'mtobias', password: 'teacher', last_name: 'Tobias', first_name: 'Margie',  RoleId: '1'},
+    {username: 'jryan', password: 'student', last_name: 'Ryan', first_name: 'Jane',  RoleId: '2', TeacherId: '1'}, 
+    {username: 'tmoreno', password: 'student', last_name: 'Moreno', first_name: 'Tom',  RoleId: '2', TeacherId: '1'},
+    {username: 'kcat', password: 'student', last_name: 'Cat', first_name: 'Katherine',  RoleId: '2', TeacherId: '1'},
+    {username: 'mmouse', password: 'student', last_name: 'Mouse', first_name: 'Mindy',  RoleId: '2', TeacherId: '2'}, 
+    {username: 'pholiday', password: 'student', last_name: 'Holiday', first_name: 'Peter',  RoleId: '2', TeacherId: '2'},
+    {username: 'mjones', password: 'student', last_name: 'Jones', first_name: 'Matina',  RoleId: '2', TeacherId: '2'},
+    {username: 'tpots', password: 'student', last_name: 'Pots', first_name: 'Tim',  RoleId: '2', TeacherId: '3'}, 
+    {username: 'pglass', password: 'student', last_name: 'Glass', first_name: 'Peter',  RoleId: '2', TeacherId: '3'},
+    {username: 'mbook', password: 'student', last_name: 'Book', first_name: 'Monica',  RoleId: '2', TeacherId: '3'}
     ],{});
   
 
